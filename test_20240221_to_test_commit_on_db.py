b06e2e2ee1e6992452c08cc243f98eb58ff78cea
miko-mm <miko-mm>
2024-02-21 15:17:53 +0000

    add the steps

--- test_20240221_to_test_commit_on_db.py
+++ test_20240221_to_test_commit_on_db.py
@@ -1,3 +1,20 @@
 # Databricks notebook source
 print("This is just a test for commit on db.")
 print("Second test.")
+"""
+Fork https://github.com/Stano-D/cicd_basic_example_public 
+
+
+fork/copy/clone repo to your own repo
+allow github action in repo
+generate DATABRICKS_TOKEN and set secrets in repo (settings -> secrets and variables -> secrets)
+set variable DATABRICKS_HOST (url of the databricks workspace)
+clone repo to the databricks
+create new branch in databricks repo
+create new change
+commit change
+create pull request
+merge to main branch in git repo
+
+Watch your triggered action in "action" (second from left in top panel in git)
+"""
